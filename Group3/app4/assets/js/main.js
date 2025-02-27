e87fa7161d6c0bf1d311b0dd9a14d58f70eca196
theclassworks <2300@mail.ir>
2025-02-27 17:23:54 +0330

    compidan

---
+++ Group3/app4/assets/js/main.js
@@ -0,0 +1,16 @@
+let totop = document.querySelector(".totop");
+
+// totop.addEventListener("click", function () {
+// //   totop.classList.add("test");
+// //   totop.classList.remove("alaki");
+//   totop.classList.toggle("alaki");
+// });
+
+window.addEventListener("scroll", function () {
+  if (this.window.scrollY > 400) {
+    totop.classList.add("active");
+  }
+  else{
+    totop.classList.remove("active");
+  }
+});
